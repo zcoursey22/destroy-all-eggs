f125b7109b3f6b7084f215e333c5c1c37f8cd018
Zach Coursey <zcoursey22@gmail.com>
2018-07-15 19:55:23 -0700

    Fix

--- bot.js
+++ bot.js
@@ -1,21 +1,22 @@
 const twit = require('twit');
-// const config = require('./config.js');
+const config = require('./config.js');
 
-let config = {
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-};
+// let config = {
+//   consumer_key: process.env.BOT_CONSUMER_KEY,
+//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//   access_token: process.env.BOT_ACCESS_TOKEN,
+//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// };
 const Twit = new twit(config);
 
 var stream = Twit.stream('statuses/filter', { track: ['DestroyAllEggs'] });
 
 var replyToAt = (tweet) => {
-  var reply = `Hello ${tweet.user.name}`;
+  var reply = `Hello @${tweet.user.screen_name}`;
 
-  if (tweet.user.profile_image_url === 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png') {
-    reply = `Don't be an egg, ${tweet.user.name}! Go give yourself a real profile picture!`;
+  console.log(tweet);
+  if (tweet.user.profile_image_url === 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png') {
+    reply = `Don't be an egg, @${tweet.user.screen_name}! Go give yourself a real profile picture!`;
   };
 
   var params = { status: reply, in_reply_to_status_id: tweet.id_str };
