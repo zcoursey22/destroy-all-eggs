09f091ef7da3a3523a5b52b3b28e81a38f2c1058
Zach Coursey <zcoursey22@gmail.com>
2018-07-15 20:04:48 -0700

    Ready for heroku

--- bot.js
+++ bot.js
@@ -1,28 +1,31 @@
 const twit = require('twit');
-const config = require('./config.js');
+// const config = require('./config.js');
 
-// let config = {
-//   consumer_key: process.env.BOT_CONSUMER_KEY,
-//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
-//   access_token: process.env.BOT_ACCESS_TOKEN,
-//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-// };
+let config = {
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+};
+
 const Twit = new twit(config);
 
-var stream = Twit.stream('statuses/filter', { track: ['DestroyAllEggs'] });
+var stream = Twit.stream('statuses/filter', { track: ['@DestroyAllEggs'] });
 
 var replyToAt = (tweet) => {
-  var reply = `Hello @${tweet.user.screen_name}`;
+  var atName = tweet.user.screen_name;
+  var tweetId = tweet.id_str;
+  var reply = `Hello @${atName}`;
 
   console.log(tweet);
   if (tweet.user.profile_image_url === 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png') {
-    reply = `Don't be an egg, @${tweet.user.screen_name}! Go give yourself a real profile picture!`;
+    reply = `Don't be an egg, @${atName}! Go give yourself a real profile picture!`;
   };
 
-  var params = { status: reply, in_reply_to_status_id: tweet.id_str };
+  var params = { status: reply, in_reply_to_status_id: tweetId };
 
   Twit.post('statuses/update', params, function(err, data, response) {
-    console.log('replied');
+    console.log('replied:', params.status);
   });
 };
 
