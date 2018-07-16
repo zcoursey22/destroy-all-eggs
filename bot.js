855fa2c7ced5606f30f3355649f207806291e51e
Zach Coursey <zcoursey22@gmail.com>
2018-07-15 21:15:57 -0700

    Solve endless loop problem

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const twit = require('twit');
-// const config = require('./config.js');
+const config = require('./config.js');
 
-let config = {
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-};
+// let config = {
+//   consumer_key: process.env.BOT_CONSUMER_KEY,
+//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//   access_token: process.env.BOT_ACCESS_TOKEN,
+//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// };
 
 const Twit = new twit(config);
 
@@ -15,18 +15,19 @@ var stream = Twit.stream('statuses/filter', { track: ['@DestroyAllEggs'] });
 var replyToAt = (tweet) => {
   var atName = tweet.user.screen_name;
   var tweetId = tweet.id_str;
-  var reply = `Hello @${atName}`;
+  var reply = `Thanks for mentioning me @${atName}! Thanks for not having a default profile picture!`;
 
-  console.log(tweet);
   if (tweet.user.profile_image_url === 'http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png') {
     reply = `Don't be an egg, @${atName}! Go give yourself a real profile picture!`;
   };
 
   var params = { status: reply, in_reply_to_status_id: tweetId };
 
-  Twit.post('statuses/update', params, function(err, data, response) {
-    console.log('replied:', params.status);
-  });
+  if (tweet.user.screen_name !== 'DestroyAllEggs') {
+    Twit.post('statuses/update', params, (err, data, response) => {
+      console.log('replied:', params.status);
+    });
+  };
 };
 
 stream.on('tweet', replyToAt);
